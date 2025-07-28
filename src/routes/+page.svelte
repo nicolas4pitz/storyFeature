8c3ced60117ae0a739adbba643e2db115d3b547c
nicolas4pitz <nicolas2004pitz@gmail.com>
2025-07-28 09:17:22 -0300

    feat: layout

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+
+
+</script>
+
+<div id="carossel" class="w-60 h-14 m-2 rounded-md border flex items-center">
+  <div id="storysadd" class="center flex ml-2">
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30px"
+    height="30px"
+    viewBox="0 0 24 24"
+    class="stroke-red-400 fill-none group-hover:fill-red-800 group-active:stroke-red-200 group-active:fill-red-600 group-active:duration-0 duration-300"
+  >
+    <path d="M8 12H16" stroke-width="1.5"></path>
+    <path d="M12 16V8" stroke-width="1.5"></path>
+  </svg>
+  </div>
+</div>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<style>
+
+  *{
+    box-sizing: border-box;
+  }
+
+  #storysadd{
+    border: 1px solid black;
+    width: 30px;
+    border-radius: 50px;
+    height: 30px;
+  }
+
+</style>
